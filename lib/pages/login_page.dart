bd711593c2ce9db1435995d4a2eaec78831744d4
Teerasej <teerasej@gmail.com>
2021-02-28 16:26:52 +0700

    Validate form and get value from inputs

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,6 +7,11 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _email = '';
+  String _password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,54 +20,80 @@ class _LoginPageState extends State<LoginPage> {
       ),
       body: Container(
         padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Email:'),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(),
-            SizedBox(
-              height: 10,
-            ),
-            Text('Password:'),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: RaisedButton(
-                color: Colors.blue,
-                textColor: Colors.white,
-                onPressed: () {},
-                child: Text('Sign in'),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Email:'),
+              SizedBox(
+                height: 10,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: OutlineButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return SignUpPage();
-                      },
-                    ),
-                  );
+              TextFormField(
+                initialValue: 'pon@gmail.com',
+                validator: (String email) {
+                  if (email.isEmpty) return 'Please fill in email';
+                  return '';
                 },
-                child: Text('Need sign up?'),
+                onSaved: (String email) {
+                  _email = email;
+                },
               ),
-            )
-          ],
+              SizedBox(
+                height: 10,
+              ),
+              Text('Password:'),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                initialValue: '1234',
+                obscureText: true,
+                validator: (String password) {
+                  if (password.isEmpty) return 'Please fill in password';
+                  return '';
+                },
+                onSaved: (String password) {
+                  _password = password;
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: RaisedButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      _formKey.currentState.save();
+                    }
+                  },
+                  child: Text('Sign in'),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: OutlineButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return SignUpPage();
+                        },
+                      ),
+                    );
+                  },
+                  child: Text('Need sign up?'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
