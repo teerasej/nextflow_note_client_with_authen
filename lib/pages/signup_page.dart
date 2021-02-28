2ad6681a17663fba005e53b76697ef591ec69bde
Teerasej <teerasej@gmail.com>
2021-02-28 18:16:07 +0700

    Get value from sign up form

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -6,6 +6,11 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _email = '';
+  String _password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,35 +19,59 @@ class _SignUpPageState extends State<SignUpPage> {
       ),
       body: Container(
         padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Email:'),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(),
-            SizedBox(
-              height: 10,
-            ),
-            Text('Password:'),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: RaisedButton(
-                color: Colors.blue,
-                textColor: Colors.white,
-                onPressed: () {},
-                child: Text('Sign up'),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Email:'),
+              SizedBox(
+                height: 10,
               ),
-            )
-          ],
+              TextFormField(
+                validator: (String email) {
+                  if (email.isEmpty) {
+                    return 'Please fill in email';
+                  }
+                  return null;
+                },
+                onSaved: (String email) {
+                  _email = email;
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text('Password:'),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                obscureText: true,
+                validator: (String password) {
+                  if (password.isEmpty) {
+                    return 'Please fill in password';
+                  }
+                  return null;
+                },
+                onSaved: (String password) {
+                  _password = password;
+                },
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: RaisedButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      _formKey.currentState.save();
+                  },
+                  child: Text('Sign up'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
