1e09368f2403a3660bb285998ba9a38b17d5e0e3
Teerasej <teerasej@gmail.com>
2021-02-28 15:41:41 +0700

    create note page

---
+++ lib/pages/note_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NotePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('บันทึก'),
+      ),
+    );
+  }
+}
