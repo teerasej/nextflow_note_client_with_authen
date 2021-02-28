9bc16b5a8e8e78eb3f642f38f6f7c8e8ebb70563
Teerasej <teerasej@gmail.com>
2021-02-28 16:54:04 +0700

    initialize dio for use in widgets

--- lib/connection.dart
+++ lib/connection.dart
@@ -1,3 +1,15 @@
+import 'package:dio/dio.dart';
+
 class Connection {
-  static String endpoint = 'https://note-api.azurewebsites.net/';
+  static String endpoint = 'https://note-api.azurewebsites.net';
+
+  static Dio getDio() {
+    var options = BaseOptions(
+      baseUrl: Connection.endpoint,
+      connectTimeout: 10000,
+    );
+
+    var dioClient = Dio(options);
+    return dioClient;
+  }
 }
