d6d2fed0a27bbe55146e820c6e63331865e7f7c3
Teerasej <teerasej@gmail.com>
2021-02-28 18:45:03 +0700

    Store token in manager if exist in using app

--- lib/token_manager.dart
+++ lib/token_manager.dart
@@ -3,17 +3,22 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class TokenManager {
   static final storage = new FlutterSecureStorage();
 
+  static String userToken;
+
   static Future<bool> isTokenExist() async {
     var token = await storage.read(key: 'token');
 
-    if (token != null && token.isNotEmpty)
+    if (token != null && token.isNotEmpty) {
+      userToken = token;
       return true;
-    else
+    } else {
       return false;
+    }
   }
 
   static Future<String> getToken() async {
     var token = await storage.read(key: 'token');
+    userToken = token;
     return token;
   }
 
