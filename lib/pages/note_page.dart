6410b336c3cda41afef564e5ae0e98cc1083946d
Teerasej <teerasej@gmail.com>
2021-02-28 18:45:32 +0700

    request and show ui related to user's notes data

--- lib/pages/note_page.dart
+++ lib/pages/note_page.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:nextflow_note_client_with_authen/connection.dart';
+import 'package:nextflow_note_client_with_authen/token_manager.dart';
 
 class NotePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var dio = Connection.getDio();
+    dio.options.headers['Authorization'] = "Bearer ${TokenManager.userToken}";
+
     return Scaffold(
       appBar: AppBar(
         title: Text('บันทึก'),
+      ),
+      body: FutureBuilder(
+        future: dio.get('/notes'),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Container();
+          }
+
+          return LinearProgressIndicator();
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
       ),
     );
   }
