f91e697d1b524dc957a32754a6cb20fd647cd44a
Teerasej <teerasej@gmail.com>
2021-02-28 17:02:10 +0700

    create token manager, easier to manage JWT token

---
+++ lib/token_manager.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class TokenManager {
+  static final storage = new FlutterSecureStorage();
+
+  static Future<bool> isTokenExist() async {
+    var token = await storage.read(key: 'token');
+
+    if (token != null || token.isNotEmpty)
+      return true;
+    else
+      return false;
+  }
+
+  static Future<String> getToken() async {
+    var token = await storage.read(key: 'token');
+    return token;
+  }
+
+  static Future<void> saveToken(String token) async {
+    await storage.write(key: 'token', value: token);
+  }
+}
